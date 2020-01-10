c15f1825df212e9af29f4a2fe7b3dadc6adb48ec
835551 <vasureddy7036373386@gmail.com>
2020-01-10 14:12:35 +0530

    sql

--- sql/data.sql
+++ sql/data.sql
@@ -1,1 +1,74 @@
 -- Include table data insertion, updation, deletion and select scripts
+
+-- -------------------------------------------------------
+
+-- Adding Movie in movie table
+-- -----------------------------------
+insert into movie
+values
+	(1, 'Avatar', 2787965087, 'Yes','2017-03-15','Science Fiction', 'Yes'),
+	(2, 'The Avangers', 1518812988, 'Yes','2017-12-23','Superhero', 'No'),
+	(3, 'Titanic', 2187463944, 'Yes','2017-08-21', 'Romance','No'),
+	(4, 'Jurassic World', 1671713208, 'No', '2017-07-02','Science Fiction', 'Yes'),
+	(5, 'Avangers:End Game', 2750760348, 'Yes','2022-11-02','Superhero', 'Yes');			
+   select * from movie ; 
+   -- -------------------------------------------------------
+-- Updating movie in movie table------------------
+-- -------------------------------------------------------
+
+update movie
+set m_title='Hero',m_box_office=306373643494,m_active='yes',m_date_of_launch='2019-12-9',m_genre='Science Fiction',m_has_teaser='yes'
+where m_id=2;
+ 
+-- -------------------------------------------------------
+-- user details in user table------------------
+-- -------------------------------------------------------
+
+insert into user(us_id,us_name)
+values
+(1,'maddy'),(2,'priya');
+select * from user;
+
+ -- -------------------------------------------------------
+-- movie table table------------------
+-- -------------------------------------------------------
+
+ select m_title,m_date_of_launch,m_genre,m_has_teaser
+ from movie
+ where m_active='yes'
+ and m_date_of_launch<(select(current_date()));
+ -- -------------------------------------------------------
+-- adding favorite movie in favorite table------------------
+-- -------------------------------------------------------
+insert into favorite(fv_id,fv_us_id,fv_pr_id)
+values
+	(1,1,3),(2,1,4);
+
+ select * from favorite;
+ -- -------------------------------------------------------
+-- -------view favorite------------------
+-- -------------------------------------------------------
+ select m_title,m_box_office,m_genre
+ from movie
+ inner join favorite 
+ on fv_pr_id=m_id
+ where fv_us_id=1;
+ 
+ select count(m_box_office) as "No_of-favorite"
+ from movie
+ inner join favorite
+ on fv_pr_id=m_id
+ where fv_us_id=1;
+  -- -------------------------------------------------------
+-- -------remove movie from favorite------------------
+-- -------------------------------------------------------
+ delete from favorite
+ where  fv_us_id=1 
+ and fv_pr_id=3;
+  -- -------------------------------------------------------
+-- -------after deleting movie from the favorite ------------------
+-- -------------------------------------------------------
+ 
+ select * from favorite;
+ 
+ 
--- sql/schema-creation.sql
+++ sql/schema-creation.sql
@@ -1,3 +1,68 @@
 -- 1. Design the schema for movie cruiser in MySQL Workbench
 -- 2. Generate the SQL schema script in MySQL Workbench
 -- 3. Paste the generated the SQL code here
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema movie crusier
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema movie crusier
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `movie_crusier` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `movie_crusier` ;
+
+-- -----------------------------------------------------
+-- Table `movie crusier`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `movie_crusier`.`user` (
+  `us_id` INT NOT NULL AUTO_INCREMENT,
+  `us_name` VARCHAR(60) NULL,
+  PRIMARY KEY (`us_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `movie crusier`.`movie`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `movie_crusier`.`movie` (
+  `m_id` INT NOT NULL AUTO_INCREMENT,
+  `m_title` VARCHAR(100) NULL,
+  `m_box_office` DECIMAL(15) NULL,
+  `m_active` VARCHAR(3) NULL,
+  `m_date_of_launch` DATE NULL,
+  `m_genre` VARCHAR(45) NULL,
+  `m_has_teaser` VARCHAR(3) NULL,
+  PRIMARY KEY (`m_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `movie crusier`.`favorite`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `movie_crusier`.`favorite` (
+  `fv_id` INT NOT NULL AUTO_INCREMENT,
+  `fv_us_id` INT NULL,
+  `fv_pr_id` INT NULL,
+  PRIMARY KEY (`fv_id`),
+  INDEX `fv_us_fk_idx` (`fv_us_id` ASC),
+  INDEX `fv_pr_fk_idx` (`fv_pr_id` ASC),
+  CONSTRAINT `fv_us_fk`
+    FOREIGN KEY (`fv_us_id`)
+    REFERENCES `movie_crusier`.`user` (`us_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fv_pr_fk`
+    FOREIGN KEY (`fv_pr_id`)
+    REFERENCES `movie_crusier`.`movie` (`m_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
